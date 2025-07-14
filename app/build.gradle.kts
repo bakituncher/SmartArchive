4d1d78c57f2f2f8cbe627ea5fae4b24cc132b189
bakituncher <tuncerbaki627@gmail.com>
2025-07-14 23:09:01 +0300

    ana durak burası gayet iyi çalışıyor ve reklam eklentileri eklendi ama tam değil tabni reklam işi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.codenzi.ceparsivi"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "3.0-admob"
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -97,4 +97,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.android.billingclient:billing-ktx:6.2.1")
 }
